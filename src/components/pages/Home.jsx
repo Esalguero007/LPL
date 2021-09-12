8d38859a09dcbba4de2f7752abf55696da0724ac
Erni Salguero <66981876+Esalguero007@users.noreply.github.com>
2021-09-12 10:26:32 -0700

    Update Home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -167,7 +167,7 @@ const Home = () => {
             <iframe
               title="La Pulguita Location"
               className="map"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3464.832383204518!2d-95.50116587075564!3d29.72461420701319!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c3b4510a6b11%3A0x28fa0c2402acba1!2s3800%20Hillcroft%20St%2C%20Houston%2C%20TX%2077057%2C%20USA!5e0!3m2!1sen!2sma!4v1623431582453!5m2!1sen!2sma"
+              src="https:"//www.google.com/maps/place/La+Pulgita+Loca/@29.7253391,-95.503823,17z/data=!4m13!1m7!3m6!1s0x8640c3b5b3cf8db7:0xf11eada67b4140e0!2s3800+Hillcroft+Ave,+Houston,+TX+77057!3b1!8m2!3d29.7253345!4d-95.5016343!3m4!1s0x8640c318d614614d:0x72792fbcbb6ec568!8m2!3d29.7246875!4d-95.501515"
               style={{ border: 0 }}
               allowFullScreen=""
               loading="lazy"
